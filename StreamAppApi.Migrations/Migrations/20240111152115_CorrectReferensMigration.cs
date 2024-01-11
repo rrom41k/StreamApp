12f47a462edd3362100b516afa1dbe6b3f003f1a
Roman Filimonov <r.filimonov03@mail.ru>
2024-01-11 23:15:01 +0300

    Update migrations

---
+++ StreamAppApi.Migrations/Migrations/20240111152115_CorrectReferensMigration.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StreamAppApi.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class CorrectReferensMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Movies_Users_UserId",
+                table: "Movies");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Movies_UserId",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Movies");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Movies",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Movies_UserId",
+                table: "Movies",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Movies_Users_UserId",
+                table: "Movies",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "_id");
+        }
+    }
+}
